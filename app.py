f3885bfc16b9fb1b0fabdd56886869ae39eee5e4
biteeunkillable <biteeunkillable@gmail.com>
2024-01-13 05:41:31 -0800

    test#3

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from modules import modules
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    content = modules.content()
+    return content
 
 
 if __name__ == "__main__":
